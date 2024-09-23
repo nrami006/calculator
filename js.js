50159cd76c086c9cade66ad0c639c5edfd032ec8
Nat <70920883+nrami006@users.noreply.github.com>
2024-09-23 12:58:58 -0700

    Populate buttons with appropriate text

--- js.js
+++ js.js
@@ -8,16 +8,39 @@ function createInputButtons() {
     inputButtonContainer.style.width = `${inputContainer.offsetWidth / input_columns}px`;
     inputButtonContainer.style.height = `${inputContainer.offsetHeight / input_rows}px`;
 
-    const inputButton = document.createElement("div");
+    const inputButton = document.createElement("button");
     inputButton.classList.add("input-button");
     inputButtonContainer.appendChild(inputButton);
-    for (let i = 0; i < input_columns; ++i) {
-        for (let j = 0; j < input_rows; ++j) {
+    for (let i = 0; i < input_rows; ++i) {
+        for (let j = 0; j < input_columns; ++j) {
             let newButton = inputButtonContainer.cloneNode(true);
+            if (j === 3) {
+                //Last column are all operators
+                newButton.firstChild.classList.add("operator-button");
+            } else if ((j < input_columns - 1 && (i > 0 && i < input_rows - 1)) || i == input_rows - 1 && j == 1) {
+                newButton.firstChild.classList.add("number-button");
+            }
             inputContainer.appendChild(newButton);
         }
     }
 }
 
+function populateButtons() {
+    const inputButtons = document.querySelectorAll(".input-button");
+    let operators = ["/", "*", "-", "+", "="];
+    let numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
+    let functions = ["AC", "+/-", "%", ":)", "."];
+
+    for (button of inputButtons) {
+        if (button.classList.contains("operator-button")) {
+            button.textContent = operators.shift();
+        }
+        else if (button.classList.contains("number-button")) {
+            button.textContent = numbers.shift();
+        } else {
+            button.textContent = functions.shift();
+        }
+    }
+}
 createInputButtons();
-// inputButton = document.createElement("div");
+populateButtons();
