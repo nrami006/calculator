7df925ca41e0728a40e79984bdb9a64f713decbb
Nat <70920883+nrami006@users.noreply.github.com>
2024-09-24 10:40:00 -0700

    Redo styling to try to match that of iOS
    Blue background on calculator display is for debugging.
    Will be removed later.

--- js.js
+++ js.js
@@ -3,31 +3,43 @@ let input_rows = 5;
 const inputContainer = document.querySelector("#calculator-input-container");
 
 function createInputButtons() {
-    const inputButtonContainer = document.createElement("div");
-    inputButtonContainer.classList.add("input-button-container");
-    inputButtonContainer.style.width = `${inputContainer.offsetWidth / input_columns}px`;
-    inputButtonContainer.style.height = `${inputContainer.offsetHeight / input_rows}px`;
+    // const inputButtonContainer = document.createElement("div");
+    // inputButtonContainer.classList.add("input-button-container");
+    // inputButtonContainer.style.maxWidth = `${inputContainer.offsetWidth / input_columns}px`;
+    // inputButtonContainer.style.height = `${inputContainer.offsetHeight / input_rows}px`;
 
     const inputButton = document.createElement("button");
     inputButton.classList.add("input-button");
-    inputButtonContainer.appendChild(inputButton);
+    inputButton.style.flexBasis = `${1 / input_rows * 100}%`;
+    // inputButton.style.maxHeight = inputButton.style.maxWidth;
+    // inputButtonContainer.appendChild(inputButton);
     for (let i = 0; i < input_rows; ++i) {
         for (let j = 0; j < input_columns; ++j) {
-            let newButton = inputButtonContainer.cloneNode(true);
+            // let newButton = inputButtonContainer.cloneNode(true);
+            let newButton = inputButton.cloneNode(true);
             if (j === 3) {
                 //Last column are all operators
-                newButton.firstChild.classList.add("operator-button");
+                // newButton.firstChild.classList.add("operator-button");
+                newButton.classList.add("operator-button");
             } else if ((j < input_columns - 1 && (i > 0 && i < input_rows - 1)) || i == input_rows - 1 && j == 1) {
-                newButton.firstChild.classList.add("number-button");
+                // newButton.firstChild.classList.add("number-button");
+                newButton.classList.add("number-button");
             }
             inputContainer.appendChild(newButton);
         }
     }
 }
 
+// Found on stackoverflow... apparently can affect legacy compatibility...but for now it works.
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
+
 function populateButtons() {
     const inputButtons = document.querySelectorAll(".input-button");
-    let operators = ["/", "*", "-", "+", "="];
+    let operators = [`${decodeHtml("&divide;")}`, `${decodeHtml("&times;")}`, `${decodeHtml("&minus;")}`, `${decodeHtml("&plus;")}`, `${decodeHtml("&equals;")}`];
     let numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
     let functions = ["AC", "+/-", "%", ":)", "."];
 
